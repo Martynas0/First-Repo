18a89a280d90c62ad0d18f7dd4ff91c023a7caf6
Martynas <siupinysm@gmail.com>
2021-03-01 18:28:36 +0000

    port

--- app.js
+++ app.js
@@ -38,6 +38,6 @@ app.post("/signup", function(req, res) {
 
 
 
-       app.listen(process.env.PORT, function() {
+       app.listen(process.env.PORT || 3000, function() {
       console.log("Server is Live !");
     })
