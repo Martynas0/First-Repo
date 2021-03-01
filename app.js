8b371730a898686ae537fc2ecee6dfb62b0809e5
Martynas <siupinysm@gmail.com>
2021-03-01 18:24:54 +0000

    Changed Port

--- app.js
+++ app.js
@@ -38,6 +38,6 @@ app.post("/signup", function(req, res) {
 
 
 
-       app.listen(3000, function() {
+       app.listen(process.env.PORT, function() {
       console.log("Server is Live !");
     })
